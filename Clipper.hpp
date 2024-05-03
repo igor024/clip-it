35ae9c18d5f8aa6542f497206b8dc80bfdb03ea4
igor024 <thedarthdot@gmail.com>
2024-05-02 22:28:43 -0400

    hide logging from ffmpeg command

--- Clipper.hpp
+++ Clipper.hpp
@@ -8,7 +8,6 @@ using namespace std;
 class Clipper {
    public:
       static void clip() {
-         string result = "";
          string c = "./clip.sh";
 
          // run command
@@ -17,6 +16,19 @@ class Clipper {
             return; //failed
          }
 
+         /* to log output 
+         string result = "";
+         char buffer[128];
+         while (!feof(pipe)) {
+
+            // use buffer to read and add to result
+            if (fgets(buffer, 128, pipe) != NULL)
+               result += buffer;
+         } 
+
+         cout << result;
+         */
+        
          pclose(pipe);
       }
 };
--- clip.sh
+++ clip.sh
@@ -6,6 +6,6 @@ input_file=$(ls -t *.wav | head -n1)
 
 length="5"
 
-ffmpeg -ss $(echo $(ffprobe -i "$input_file" -show_entries format=duration -v quiet -of csv="p=0") - "$length" | bc ) -t 30 -i "$input_file" temp.wav  -y  && mv temp.wav "$input_file"
+ffmpeg -loglevel quiet -ss $(echo $(ffprobe -i "$input_file" -show_entries format=duration -v quiet -of csv="p=0") - "$length" | bc ) -t 30 -i "$input_file" temp.wav  -y  && mv temp.wav "$input_file"
 
 cd ..
